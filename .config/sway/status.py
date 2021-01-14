d8547930f3115e30f1bce70e77b351eed5e666f7
Daniel Woffinden <daw@google.com>
2021-01-14 20:37:00 +0000

    Just use a format string

--- .config/sway/status.py
+++ .config/sway/status.py
@@ -10,18 +10,19 @@ while True:
     # TODO: less awful code, inefficiency is probs fine
     # TODO: colours? https://man.archlinux.org/man/swaybar-protocol.7.en
     # TODO: net traffic?
+    # TODO: Lil' history graphs? Click to expand?
 
     # TODO: AC/not 🔌⚡, charge/discharge time
-    bat = f"{psutil.sensors_battery().percent:.0f}"
+    bat = psutil.sensors_battery().percent
 
     # TODO: load avg? frequency? pegged cores?
     cpu = str(psutil.cpu_percent(interval=None))
 
     # TODO: be more dynamic? include more sensors?
-    temp = f"{psutil.sensors_temperatures()['coretemp'][0].current:.0f}"
+    temp = psutil.sensors_temperatures()['coretemp'][0].current
 
     time = datetime.now().strftime('%Y-%m-%d %k:%M:%S')
 
-    line = '[{"full_text":"🔋' + bat + '%"},{"full_text":"CPU ' + cpu + '%"},{"full_text":"🌡️' + temp + '°C"},{"full_text":"' + time + '"}],'
+    line = f'[{{"full_text":"🔋{bat:.0f}%"}},{{"full_text":"CPU {cpu}%"}},{{"full_text":"🌡️{temp:.0f}°C"}},{{"full_text":"{time}"}}],'
     print(line, end='', flush=True)
     sleep(1)
