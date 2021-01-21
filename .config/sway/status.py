4151c9a21650c054b16e456a3c0ab7c5ab694c85
Daniel Woffinden <daw@google.com>
2021-01-21 16:55:05 +0000

    rudimentary net speed status

--- .config/sway/status.py
+++ .config/sway/status.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
 
 from datetime import datetime
+from psutil._common import bytes2human
 from time import sleep
 
 import psutil
+
+def net_diff(old):
+    stats = psutil.net_io_counters(nowrap=True)
+    return stats, stats.bytes_sent - old.bytes_sent, stats.bytes_recv - old.bytes_recv
+
+def bps(n):
+    return bytes2human(n) + '/s'
+
+net_stats = psutil.net_io_counters(nowrap=True)
 
 print('{"version":1}\n[', end='')
 while True:
     # TODO: less awful code, inefficiency is probs fine
     # TODO: colours? https://man.archlinux.org/man/swaybar-protocol.7.en
-    # TODO: net traffic?
     # TODO: Lil' history graphs? Click to expand?
 
     # TODO: AC/not 🔌⚡, charge/discharge time
     bat = psutil.sensors_battery().percent
+
+    # TODO: eth vs wifi SSID? latency? strength? errors/drops?
+    net_stats, netup, netdn = net_diff(net_stats)
 
     # TODO: load avg? frequency? pegged cores?
     cpu = str(psutil.cpu_percent(interval=None))
@@ -23,6 +35,6 @@ while True:
 
     time = datetime.now().strftime('%Y-%m-%d %k:%M:%S')
 
-    line = f'[{{"full_text":"🔋{bat:.0f}%"}},{{"full_text":"CPU {cpu}%"}},{{"full_text":"🌡️{temp:.0f}°C"}},{{"full_text":"{time}"}}],'
+    line = f'[{{"full_text":"⬆️{bps(netup)}⬇️{bps(netdn)}"}},{{"full_text":"🔋{bat:.0f}%"}},{{"full_text":"CPU {cpu}%"}},{{"full_text":"🌡️{temp:.0f}°C"}},{{"full_text":"{time}"}}],'
     print(line, end='', flush=True)
     sleep(1)
