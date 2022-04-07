28d1914388dec2538f4db46f0c57b61a259b5fa3
Daniel Woffinden <daw@google.com>
2022-04-07 10:57:52 +0100

    Finally clicked inhibit startup screen

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -16,3 +16,16 @@
 ; (add-hook 'org-mode-hook #'toggle-word-wrap)
 
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
